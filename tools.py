c9dfdebce4308e6fdf6661126d7aef54634b3076
Richard Liu <hoffmanlabcoding@gmail.com>
2024-03-29 10:47:55 -0400

    Added load_ibw() in tools.py

--- tools.py
+++ tools.py
@@ -1,6 +1,10 @@
 import numpy as np
 import scipy as sp
 
+import matplotlib.pyplot as plt
+import os
+from igor2 import binarywave as bw
+
 
 def lineSubtract(data, n=1, maskon=False, thres=4, M=4, normalize=True, colSubtract=False):
     '''
@@ -213,3 +217,179 @@ def fft(dataIn, window='None', output='absolute', zeroDC=False, beta=1.0,
                 print('WARNING: The window function "%s" messes up the FT units' %
                         window)
     return ftData
+
+# User defined functions
+
+# @title
+class IBWData:
+    def __init__(self, header=None, channels=None, data=None, size=None):
+        self.header = header if header is not None else {}
+        self.channels = channels if channels is not None else []
+        self.data = data if data is not None else []
+
+    def __repr__(self):
+        return f"IBWData(header={self.header}, channels={self.channels}, data={self.data})"
+
+def load_ibw(file, ss=False):
+    '''
+    Load the ibw file into an IBWData object.
+    It automatically creates three default attributes:
+      1. header: a dict contains all the setup information
+      2. channels: a list of channel names for each image data
+      3. data: an array of all the saved image data in this ibw file
+    '''
+    t = bw.load(file)
+    wave = t.get('wave')
+
+    # Decode the notes section to parse the header
+    if isinstance(wave['note'], bytes):
+        try:
+            parsed_string = wave['note'].decode('utf-8').split('\r')
+        except:
+            parsed_string = wave['note'].decode('ISO-8859-1').split('\r')
+    header = {}
+    for item in parsed_string:
+        try:
+            key, value = item.split(':', 1)
+            value = value.strip()  # Remove leading/trailing whitespace
+        except ValueError:
+            continue  # For items that do not split correctly
+
+        # Determine the data type of the value and convert
+        if '.' in value or 'e' in value:  # Floating point check
+            try:
+                header[key] = float(value)
+            except ValueError:
+                header[key] = value
+        elif value.lstrip('-').isdigit():  # Integer check
+            header[key] = int(value)
+        else:
+            header[key] = value
+
+    # Transpose the data matrix
+    data = wave['wData'].T
+    # data = wave['wData']
+
+    # Extract channel data types from the header
+    channels = [header.get(f'Channel{i+1}DataType', 'Unknown') for i in range(np.shape(data)[0])]
+    out = IBWData(header, channels, data)
+    out.size = header['ScanSize']
+    out.mode = header['ImagingMode']
+    if out.mode == "PFM Mode":
+        if len(out.channels) > 4:
+            out.mode = "DART Mode"
+            out.channels = ['Height', 'Amplitude1', 'Amplitude2', 'Phase1', 'Phase2', 'Frequency']
+        else:
+            out.channels = ['Height', 'Amplitude', 'Deflection', 'Phase']
+
+    # Load the switching spectroscopy (hysteresis loop) data
+    if ss is not False:
+        bias_raw= data[-1]
+        index_values = np.where(~np.isnan(bias_raw))
+
+        bias = bias_raw[index_values]
+        amp, phase1, phase2 = data[2][index_values], data[3][index_values], data[4][index_values]
+        index_bp = np.where(np.diff(bias) != 0)[0] + 1
+        index_bp = np.concatenate([[0], index_bp])
+
+        phase1 = correct_phase_wrapping(phase1)
+        phase2 = correct_phase_wrapping(phase2)
+
+        length = len(index_bp) // 2
+
+        bias_on = np.zeros(length)
+        bias_off = np.zeros(length)
+
+        phase1_on = np.zeros(length)
+        phase1_off = np.zeros(length)
+
+        phase2_on = np.zeros(length)
+        phase2_off = np.zeros(length)
+
+        amp_on = np.zeros(length)
+        amp_off = np.zeros(length)
+
+        for i in range(length * 2-1):
+            if i % 2 == 0: # bias off
+                phase1_off[i//2] = np.mean(phase1[index_bp[i]:index_bp[i+1]])
+                phase2_off[i//2] = np.mean(phase2[index_bp[i]:index_bp[i+1]])
+                amp_off[i//2] = np.mean(amp[index_bp[i]:index_bp[i+1]])
+                bias_off[i//2] = np.mean(bias[index_bp[i]:index_bp[i+1]])
+            else:
+                bias_on[i//2] = np.mean(bias[index_bp[i]:index_bp[i+1]])
+                phase1_on[i//2] = np.mean(phase1[index_bp[i]:index_bp[i+1]])
+                phase2_on[i//2] = np.mean(phase2[index_bp[i]:index_bp[i+1]])
+                amp_on[i//2] = np.mean(amp[index_bp[i]:index_bp[i+1]])
+        out.bias = bias_on[1:]
+        out.phase1_on = phase1_on[1:]
+        out.phase1_off = phase1_off[1:]
+        out.phase2_on = phase2_on[1:]
+        out.phase2_off = phase2_off[1:]
+        out.amp_on = amp_on[1:] * np.cos(phase1_off[1:]/180*np.pi)
+        out.amp_off = amp_off[1:] * np.cos(phase1_off[1:]/180*np.pi)
+        out.data = data
+    # Return an IBWData object
+    return out
+
+# load functions
+def ss_loop(A, nan=False):
+
+    if nan is True:
+        bias_raw= A[-1]
+        index_values = np.where(~np.isnan(bias_raw))
+
+        bias = bias_raw[index_values]
+        amp, phase1, phase2 = A[2][index_values], A[3][index_values], A[4][index_values]
+    else:
+        bias = A[-1]
+        amp = A[2]
+        phase1 = A[3]
+        phase2 = A[4]
+    phase1 = correct_phase_wrapping(phase1)
+    phase2 = correct_phase_wrapping(phase2)
+
+    index_bp = np.where(np.diff(bias) != 0)[0] + 1
+    index_bp = np.concatenate([[0], index_bp])
+
+    length = len(index_bp) // 2
+
+    bias_on = np.zeros(length)
+    bias_off = np.zeros(length)
+
+    phase1_on = np.zeros(length)
+    phase1_off = np.zeros(length)
+
+    phase2_on = np.zeros(length)
+    phase2_off = np.zeros(length)
+
+    amp_on = np.zeros(length)
+    amp_off = np.zeros(length)
+
+    for i in range(length * 2-1):
+        if i % 2 == 0: # bias off
+            phase1_off[i//2] = np.mean(phase1[index_bp[i]:index_bp[i+1]])
+            phase2_off[i//2] = np.mean(phase2[index_bp[i]:index_bp[i+1]])
+            amp_off[i//2] = np.mean(amp[index_bp[i]:index_bp[i+1]])
+            bias_off[i//2] = np.mean(bias[index_bp[i]:index_bp[i+1]])
+        else:
+            bias_on[i//2] = np.mean(bias[index_bp[i]:index_bp[i+1]])
+            phase1_on[i//2] = np.mean(phase1[index_bp[i]:index_bp[i+1]])
+            phase2_on[i//2] = np.mean(phase2[index_bp[i]:index_bp[i+1]])
+            amp_on[i//2] = np.mean(amp[index_bp[i]:index_bp[i+1]])
+    bias = bias_on
+    phase1_on = phase1_on
+    phase1_off = phase1_off
+    phase2_on = phase2_on
+    phase2_off = phase2_off
+    amp_on = amp_on * np.cos(phase2_off/180*np.pi)
+    amp_off = amp_off * np.cos(phase1_off/180*np.pi)
+    return bias[1:], amp_off[1:], phase1_off[1:], phase2_off[1:]
+
+
+def correct_phase_wrapping(ph):
+    ph_shift = ph - ph[-1]
+    index1 = np.where(ph_shift > 270)
+    index2  =np.where(ph_shift < -90)
+    ph_shift[index1] -= 360
+    ph_shift[index2] += 360
+    return ph_shift
