9c202d2afbc9c2bb6cc39234e0e1c12ca5691f8f
RichardLiuCoding <42445314+RichardLiuCoding@users.noreply.github.com>
2025-06-04 14:45:19 -0400

    More keywords for spm_control() and tools.

--- aespm/experiment.py
+++ aespm/experiment.py
@@ -475,11 +475,21 @@ def spm_control(action, value=None, wait=0.35, connection=None):
         ['ZeroPD', 'PDZero'],
         ['DARTTrigger', 'SSTrigger'],
         ['DARTAmp', 'DART v_ac'],
-        ['SampleHeight', 'Sample Height'], #54
-        ['SingleTune', 'TuneOnce', 'ACTune'],
-        ['SinglePhase', 'PhaseOnce', 'ACPhase'],
+        ['SampleHeight', 'Sample Height'], # 54
+        ['DARTIGain', 'DART i_gain', 'DART_I_Gain', 'DART I Gain'],
+        ['Arg4', 'ARG 4', 'arg4', 'ARG4'],
+        ['SamplingFreq', 'Sampling Freq'],
+        # KPFM related
+        ['Trigger Point', 'TriggerPoint', 'ElectricalTuneTrigger'],
+        ['NapTipVoltage', 'TipVoltageNap'], # 59
+        ['Potential I Gain', 'PotentialIGain', 'ElectricalIGain', 'Electrical I Gain'],
+        ['Potential P Gain', 'PotentialPGain', 'ElectricalPGain', 'Electrical P Gain'],
+        ['ElectricalTune', 'ElectricalTuneOnce'],
+        ['ElectricalTuneCenter', 'ElectricalTuneCenterPhase'],
+        ['ElectricalSingleForce', 'NapSingleForce'], # 64
+        ['GetForce'],
+        ['ZeroPhase', 'SinglePhase', 'SinglePhase90'],
         ['ChangeFunction', 'DARTFunction', 'IVFunction'],
-        
     ]
 
     value_list = [
@@ -496,6 +506,7 @@ def spm_control(action, value=None, wait=0.35, connection=None):
         ['GoForce_1', 'MasterPanel', 0],
         ['TipVoltageSetVar_0', 'NapPanel', 1],
         ['SurfaceVoltageSetVar_0', 'NapPanel', 1],
+        # ['SingleForce_1', 'MasterPanel', 0],
         ['SingleForce()', 4],
         ["EnableStageCB_0","MasterMotorPanel", 1, 5], # 14
         ["MotorEngageButton_0","MasterMotorPanel",0], 
@@ -537,10 +548,22 @@ def spm_control(action, value=None, wait=0.35, connection=None):
         ["ZeroLDPDButton_1","MasterMotorPanel",0], 
         ["TriggerPointSetVar_2","DARTSpectroscopy", 1],
         ["DriveAmplitudeSetVar_3", "DART", 1], 
-        ['PV("SampleHeight", {})'.format(value), 4], #54
-        ['SingleTune()', 4],
+        ['PV("SampleHeight", {})'.format(value), 4], # 54
+        ["DARTIGainSetVar_0", "DART", 1], 
+        ["ARDoIVArg3SetVar_1","DARTSpectroscopy", 1],
+        ["NumPtsPerSecSetVar_2","DARTSpectroscopy", 1],
+        # KPFM related
+        ["TriggerPointSetVar_0","ElectricPanel", 1],
+        ["NapTipVoltageSetVar_0","ElectricPanel", 1], # 59
+        ["PotentialIGainSetVar_0","ElectricPanel", 1],
+        ["PotentialPGainSetVar_0","ElectricPanel", 1],
+        ["DoTuneElectric_0","ElectricPanel", 0],
+        ["DoTuneCent_0","ElectricPanel", 0],
+        ["SingleForce_0","ElectricPanel", 0], # 64
+        ['GetForce()', 4],
         ['SinglePhase()', 4],
         ['ARDoIVPopFunc("ARDoIVFuncPop_1",{},"ARDoIVFuncPop_1")'.format(value), 4],
+        
     ]
     # Construct the action dict
     for i, key in enumerate(key_list):
@@ -637,7 +660,7 @@ def move_stage(distance, connection=None):
         write_spm(commands='MoveStage("{}")'.format(y_direction), connection=connection)
 
 
-def tune_probe(num=1, path=os.path.join(_buffer_path, 'Tune.ibw'), center=None, width=50e3, out=False, connection=None):
+def tune_probe(num=1, path=os.path.join(_buffer_path, 'Tune.ibw'), center=None, width=50e3, out=False, readonly=False, connection=None):
     '''
     Tune the probe in the DART mode and optionally read the tune data.
 
@@ -647,35 +670,45 @@ def tune_probe(num=1, path=os.path.join(_buffer_path, 'Tune.ibw'), center=None, 
         center  - float: center of the tuning frequency range
         width   - float: frequency range of runing
         out     - Boolean: flag to indicate if tune data will be output
+        readonly- Boolean: flag to indicate if tune will be read only or after retune
     Output:
         tune    - an AR wave containing frequency and amplitude
     Example:
         w = ae.tune_probe(num=2, out=True)
     '''
-    for i in range(num):
-        spm_control('OneTuneDART', wait=1, connection=connection)
+    if readonly is False:
+        for i in range(num):
+            spm_control('OneTuneDART', wait=1, connection=connection)
+            spm_control('GetTune', wait=0.5, connection=connection)
+            if connection is not None:
+                aespm.utils.download_file(connection=connection, file_path=path, local_file_name='tune.ibw')
+                w = ibw_read('tune.ibw').data
+            else:
+                w = ibw_read(path).data
+            freq = w[0][w[1].argmax()]
+            if center is not None:
+                if abs(freq-center) > width:
+                    freq = center
+            spm_control('DARTFreq', value=freq, connection=connection)
         spm_control('GetTune', wait=0.5, connection=connection)
         if connection is not None:
             aespm.utils.download_file(connection=connection, file_path=path, local_file_name='tune.ibw')
             w = ibw_read('tune.ibw').data
         else:
             w = ibw_read(path).data
+        # Get the freq corresponds to the max intensity
         freq = w[0][w[1].argmax()]
-        if center is not None:
-            if abs(freq-center) > width:
-                freq = center
+        # Set this freq to be the driven freq
         spm_control('DARTFreq', value=freq, connection=connection)
-    spm_control('GetTune', wait=0.5, connection=connection)
-    if connection is not None:
-        aespm.utils.download_file(connection=connection, file_path=path, local_file_name='tune.ibw')
-        w = ibw_read('tune.ibw').data
+        spm_control('CenterPhase', connection=connection)
     else:
-        w = ibw_read(path).data
-    # Get the freq corresponds to the max intensity
-    freq = w[0][w[1].argmax()]
-    # Set this freq to be the driven freq
-    spm_control('DARTFreq', value=freq, connection=connection)
-    spm_control('CenterPhase', connection=connection)
+        spm_control('GetTune', wait=0.5, connection=connection)
+        if connection is not None:
+            aespm.utils.download_file(connection=connection, file_path=path, local_file_name='tune.ibw')
+            w = ibw_read('tune.ibw').data
+        else:
+            w = ibw_read(path).data
+            
     if out == True:
         return w
 
